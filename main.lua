8cd281942a462e3d139621371a5fe8261baf00be
Blazoned <bfjkroon@ziggo.nl>
2025-01-13 23:12:26 +0100

    Added sussy hello world
    - Restructured main script, and added sound effects for a level transitioning and dying (sounds have been excluded due to copyright)

--- main.lua
+++ main.lua
@@ -1,42 +1,61 @@
--- Register the mod, which grants the ability to add code that correspond to in-game events (i.e. "callbacks").
-local mod = RegisterMod("Hello Isaac", 1)
+--#region Main (Hello Isaac)
+local mod = RegisterMod("Reinforcement of Isaac", 1)
 
-local function helloIsaac(npcEntity)
-  print("Hello, Isaac!")
+local sfxManager = SFXManager()
+local roundStartSfx = Isaac.GetSoundIdByName("Round Start")
+local gameOverSfx = Isaac.GetSoundIdByName("Game Over")
+
+sfxManager:Preload(roundStartSfx)
+sfxManager:Preload(gameOverSfx)
+--#endregion
+
+--#region Callbacks
+local function helloIsaac()
+  print("[RoI] Hello, Isaac!" .. tostring(roundStartSfx))
+  sfxManager:Play(roundStartSfx, 100)
 end
 
-local function handleUserIntervention()
+--#region Inputs Callbacks
+local function HandleUserInput()
 
 end
 
-local function insertReinforcementAgentAction(entity, inputHook, buttonAction)
+local function CallAgentAction(entity, inputHook, buttonAction)
   -- Return bool when inputHook is Input.IsActionTriggered or Input.IsActionPressed, return float when inputHook is Input.IsActionValue
 end
+--#endregion
 
-local function gameStart(isContinued)
-
+--#region Game Event Callbacks
+local function GameStart(isContinued)
+  print("[RoI] Welcome!! " .. tostring(roundStartSfx))
+  sfxManager:Play(roundStartSfx, 100)
 end
 
-local function gameEnd(isLost)
-
+local function GameEnd(isLost)
+  if isLost then
+    print("[RoI] Failure... " .. tostring(gameOverSfx))
+    sfxManager:Play(gameOverSfx, 100)
+  end
 end
 
-local function gameExit(doSave)
+local function GameExit(doSave)
 
 end
+--#endregion
+--#endregion
 
 -- HELLO ISAAC :D
-mod:AddPriorityCallback(ModCallbacks.MC_NPC_UPDATE, CallbackPriority.IMPORTANT, helloIsaac)
+mod:AddPriorityCallback(ModCallbacks.MC_POST_NEW_LEVEL, CallbackPriority.IMPORTANT, helloIsaac)
 
 -- Frequent agent updates have 3 rates to use (the latter is used for overriding inputs):
 -- * MC_POST_UPDATE -> 30 times per second (not when paused)
 -- * MC_POST_RENDER -> 60 times per second (also when paused)
 -- * MC_INPUT_ACTION -> ~49 times per logic step; called every time the game polls for a ButtonAction and can be used to change inputs.
 
-mod:AddPriorityCallback(ModCallbacks.MC_POST_RENDER, CallbackPriority.IMPORTANT, handleUserIntervention)
-mod:AddPriorityCallback(ModCallbacks.MC_INPUT_ACTION, CallbackPriority.IMPORTANT, insertReinforcementAgentAction)
+mod:AddPriorityCallback(ModCallbacks.MC_POST_RENDER, CallbackPriority.IMPORTANT, HandleUserInput)
+mod:AddPriorityCallback(ModCallbacks.MC_INPUT_ACTION, CallbackPriority.IMPORTANT, CallAgentAction)
 
 -- Add agent triggers to start and stop reinforcement agents from the mod itself
-mod:AddPriorityCallback(ModCallbacks.MC_POST_GAME_STARTED, CallbackPriority.IMPORTANT, gameStart)
-mod:AddPriorityCallback(ModCallbacks.MC_POST_GAME_END, CallbackPriority.IMPORTANT, gameEnd)
-mod:AddPriorityCallback(ModCallbacks.MC_PRE_GAME_EXIT, CallbackPriority.IMPORTANT, gameExit)
+mod:AddPriorityCallback(ModCallbacks.MC_POST_GAME_STARTED, CallbackPriority.IMPORTANT, GameStart)
+mod:AddPriorityCallback(ModCallbacks.MC_POST_GAME_END, CallbackPriority.IMPORTANT, GameEnd)
+mod:AddPriorityCallback(ModCallbacks.MC_PRE_GAME_EXIT, CallbackPriority.IMPORTANT, GameExit)
